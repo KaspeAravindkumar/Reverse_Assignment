200c9f14eb903dda651a313ab5e7a71fef3a5067
Kaspe Aravind Kumar <kaspe.kumar@knoldus.com>
2021-03-12 09:54:54 +0530

    Assignment done by Aravind kumar

--- Reverse/src/test/scala/com/knoldus/ReverseTest.scala
+++ Reverse/src/test/scala/com/knoldus/ReverseTest.scala
@@ -1,12 +1,13 @@
 package com.knoldus
+
 import org.scalatest.flatspec.AnyFlatSpec
 class ReverseTest extends AnyFlatSpec {
   val reverse = new Reverse
 
   //TestCase for Reverse String
   "reverse of string" should "be found"in {
-    val x = reverse.reverseString("aravind")
-    assert(x == "dnivara")
+    val x = reverse.reverseString("Arun")
+    assert(x == "nurA")
   }
 
   //TestCase for Reverse List of Integers
@@ -19,6 +20,18 @@ class ReverseTest extends AnyFlatSpec {
   "reverse list of Strings" should "be found" in{
     val res=reverse.reverseList(List("one","two","three","four"))
     assert(res==List("four","three","two","one"))
+  }
+
+  //TestCase for Reverse list of Any Data type
+  "reverse list of any data type" should "be found" in{
+    val res=reverse.reverseList(List("one",2,"three",4))
+    assert(res==List(4,"three",2,"one"))
+  }
+
+  //Testcase should fail as the excpeted is wrong
+  "Negative test case" should "be found" in{
+    val res=reverse.reverseList(List("one",2.78,"three",4))
+    assert(!(res==List(4,"three",2.78,1)))
   }
 
 
